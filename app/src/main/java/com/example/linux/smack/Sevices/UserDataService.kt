81d004074b85d55a45595d29726091e282fb22da
Rodrigo Calderano Barbacovi <rodrigo.calderano@gmail.com>
2018-09-25 18:04:31 -0300

    Broadcast created

--- app/src/main/java/com/example/linux/smack/Sevices/UserDataService.kt
+++ app/src/main/java/com/example/linux/smack/Sevices/UserDataService.kt
@@ -1,5 +1,8 @@
 package com.example.linux.smack.Sevices
 
+import android.graphics.Color
+import java.util.*
+
 object UserDataService {
 
     var id = ""
@@ -7,4 +10,32 @@ object UserDataService {
     var avatarName = ""
     var email = ""
     var name = ""
+
+    fun logout(){
+        var id = ""
+        var avatarColor = ""
+        var avatarName = ""
+        var email = ""
+        var name = ""
+        AuthService.authToken = ""
+        AuthService.userEmail = ""
+        AuthService.isLoggedIn = false
+    }
+
+    fun returnAvatarColor(components : String) : Int {
+        val strippedColor = components.replace("[", "")
+                .replace("]", "")
+                .replace(",", "")
+        var r = 0
+        var g = 0
+        var b = 0
+        val scanner = Scanner(strippedColor)
+        if (scanner.hasNext()){
+            r = (scanner.nextDouble()).toInt()
+            g = (scanner.nextDouble()).toInt()
+            b = (scanner.nextDouble()).toInt()
+        }
+        return Color.rgb(r, g, b)
+    }
+
 }
