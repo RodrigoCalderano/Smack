500daa2cdcd0cbfffa17c1ed8731ef9dd9373f9c
Rodrigo Calderano Barbacovi <rodrigo.calderano@gmail.com>
2018-09-25 15:43:29 -0300

    Creating User working

---
+++ app/src/main/java/com/example/linux/smack/Sevices/UserDataService.kt
@@ -0,0 +1,10 @@
+package com.example.linux.smack.Sevices
+
+object UserDataService {
+
+    var id = ""
+    var avatarColor = ""
+    var avatarName = ""
+    var email = ""
+    var name = ""
+}
--- app/src/main/java/com/example/linux/smack/Utilities/Constants.kt
+++ app/src/main/java/com/example/linux/smack/Utilities/Constants.kt
@@ -3,3 +3,5 @@ package com.example.linux.smack.Utilities
 const val BASE_URL = "https://smack-messenger.herokuapp.com/v1/"
 const val URL_REGISTER = "${BASE_URL}account/register"
 const val URL_LOGIN = "${BASE_URL}account/login"
+const val URL_CREATE_USER = "${BASE_URL}user/add"
+
