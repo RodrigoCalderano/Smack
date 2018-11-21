e3caced8a64a95d468cd2865eed636f803226c48
Rodrigo Calderano Barbacovi <rodrigo.calderano@gmail.com>
2018-11-21 19:08:53 -0200

    cardView adicionado na lista de mensagens

--- app/src/main/java/com/example/linux/smack/Adapters/MessageAdapter.kt
+++ app/src/main/java/com/example/linux/smack/Adapters/MessageAdapter.kt
@@ -18,7 +18,7 @@ import java.util.*
 class MessageAdapter(val context : Context, val messages: ArrayList<SmackMessage>) : RecyclerView.Adapter<MessageAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(context).inflate(R.layout.message_list_view, parent, false)
+        val view = LayoutInflater.from(context).inflate(R.layout.message_item_recycler, parent, false)
         return ViewHolder(view)
     }
 
@@ -27,14 +27,14 @@ class MessageAdapter(val context : Context, val messages: ArrayList<SmackMessage
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder?.bindMessages(context, messages[position])
+        holder.bindMessages(context, messages[position])
     }
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val userImage = itemView?.findViewById<ImageView>(R.id.messageUserImage)
-        val timeStamp = itemView?.findViewById<TextView>(R.id.timeStampLabel)
-        val userName = itemView?.findViewById<TextView>(R.id.messageUserNameLabel)
-        val messageBody = itemView?.findViewById<TextView>(R.id.messageBodyLabel)
+        val userImage = itemView.findViewById<ImageView>(R.id.messageUserImage)
+        val timeStamp = itemView.findViewById<TextView>(R.id.timeStampLabel)
+        val userName = itemView.findViewById<TextView>(R.id.messageUserNameLabel)
+        val messageBody = itemView.findViewById<TextView>(R.id.messageBodyLabel)
 
         fun bindMessages(context: Context, message: SmackMessage) {
             val resourceId = context.resources.getIdentifier(message.userAvatar,
